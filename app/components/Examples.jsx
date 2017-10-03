d3ba191458da9ba7652b1e2a0f2de4d0df84d39f
Ajay sherawat <sherawatajay4@gmail.com>
2017-10-03 14:59:45 +0530

    Added grid to main and updated examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>this is example</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations</p>
+      <ol>
+        <li>
+          <Link to='/?location=Jaipur'>Jaipur, Rajasthan</Link>
+        </li>
+        <li>
+          <Link to='/?location=Silchar'>Silchar, Assam</Link>
+        </li>
+      </ol>
     </div>
   )
 };
