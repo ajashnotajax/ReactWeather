2f6ef2dc4fc3bc04d2a660c938ccc1c6c419899e
Ajay sherawat <sherawatajay4@gmail.com>
2017-10-04 00:00:11 +0530

    Wired nav and examples

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,9 +4,15 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function(e){
     e.preventDefault();
-    alert('not wired');
-  },
+    var location = this.refs.location.value;
+    var encodedLocation = encodeURIComponent(location);
 
+    if(location.length > 0){
+      this.refs.location.value = '',
+      window.location.hash = '#/?location=' + encodedLocation;
+    }
+  },
+  
   render:function(){
     return(
       <div className="top-bar">
@@ -29,7 +35,7 @@ var Nav = React.createClass({
         <div className="top-bar-right">
           <form onSubmit={this.onSearch}>
             <ul className="menu">
-              <li><input type="search" placeholder="Enter City Name"/></li>
+              <li><input type="search" placeholder="Enter City Name" ref='location'/></li>
               <li><input type="submit" className="button" value="Get Weather"/></li>
             </ul>
 
