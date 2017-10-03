a3f31380d890ca3b7260d99b99516be465f3261a
Ajay sherawat <sherawatajay4@gmail.com>
2017-10-03 15:43:05 +0530

    Updated Weather and WeatherForm

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -19,7 +19,7 @@ var WeatherForm = React.createClass({
             <input type="text" placeholder="Name of location" ref="location"/>
           </div>
           <div>
-            <button>Get Weather</button>
+            <button className="hollow button expanded">Get Weather</button>
           </div>
         </form>
       </div>
